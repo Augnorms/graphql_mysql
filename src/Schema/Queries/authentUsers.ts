7a2f9409f6f83e5da6e5aa934d9913602a367184
Augustine Normanyo <augustine.normanyo@amalitech.com>
2023-06-01 08:29:16 +0100

    updated schema

--- src/Schema/Queries/authentUsers.ts
+++ src/Schema/Queries/authentUsers.ts
@@ -1,34 +1,32 @@
 import { Users } from '../../Mysql_Entities/TableUsers';
-import { GraphQLString} from 'graphql';
+import { GraphQLString } from 'graphql';
 import { Messages } from '../Db_table_TypeDefs/Mesaages';
-import {comparePassword, generateToken} from  '../../auth'
+import { comparePassword, generateToken } from '../../auth';
 
 export const AUTHENTICATE_USER = {
-    type: Messages,
-    args: {
-      username: { type: GraphQLString },
-      password: { type: GraphQLString },
-    },
-    async resolve(parent: any, args: any) {
-      const { username, password } = args;
-  
-      const user = await Users.findOne({ where: { username } });
-  
-      if (!user || !(await comparePassword(password, user.password))) {
-        return {
-          failure: true,
-          Messages: 'Invalid username or password',
-        };
-      }
-  
-      const token = generateToken({ id: String(user.id) });
-  
+  type: Messages,
+  args: {
+    username: { type: GraphQLString },
+    password: { type: GraphQLString },
+  },
+  async resolve(parent: any, args: any) {
+    const { username, password } = args;
+
+    const user = await Users.findOne({ where: { username } });
+
+    if (!user || !(await comparePassword(password, user.password))) {
       return {
-        success: true,
-        failure: false,
-        Messages: 'Authentication successful',
-        token,
+        success: false,
+        Messages: 'Invalid username or password',
       };
-    },
-  };
-  
+    }
+
+    const token = generateToken({ id: String(user.id) });
+
+    return {
+      success: true,
+      Messages: 'Authentication successful',
+      token,
+    };
+  },
+};
